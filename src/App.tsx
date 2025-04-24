7cdec84a7a6a3e9dafaf354aef95fb214ba8565b
Aleksandar Dimitrov <alexanderdimitrov333@gmail.com>
2025-04-24 15:26:00 +0300

    missed file save

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import CustomWpTabComp from './CustomWpTabComp';
 
 const App = () => {
     (window as any).io = useContext(IOConnectContext);
+    const io = useContext(IOConnectContext);
 
     useEffect(() => {
         const shortcuts: { [id: string]: () => void } = {};
